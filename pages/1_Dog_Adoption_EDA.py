2f6a9b1165d9d02004a5ee4df04739605e5c8fb5
jumpingchu <jumpingchu@gmail.com>
2023-12-21 19:15:56 +0800

    Optimize code

--- pages/1_Dog_Adoption_EDA.py
+++ pages/1_Dog_Adoption_EDA.py
@@ -25,11 +25,13 @@ st.dataframe(
     },
 )
 
-## 時間分析
 dog_data_plotter = DogDataPlotter(df)
-tab_year, tab_month = st.tabs(["每年分佈", "每月分佈"])
+
+## 時間分析
+tab_list = ["每年分佈", "每月分佈"]
+tab_year, tab_month = st.tabs(tab_list)
 with tab_year:
-    fig = dog_data_plotter.yearly_plot()
+    fig = dog_data_plotter.yearly_bar(title=tab_list[0])
     md_text_list = [
         "##",
         "- 2023 年明顯遠高於其他年份，但無法確定是因為流浪動物今年大幅增加，還是政府今年開始積極登記流浪動物，需待更多資訊才能確認此原因。",
@@ -37,26 +39,25 @@ with tab_year:
     col_left_right_component(fig, md_text_list)
 
 with tab_month:
-    fig = dog_data_plotter.monthly_plot()
+    fig = dog_data_plotter.monthly_bar(title=tab_list[1])
     md_text_list = [
         "- 整體來看，會發現 9~12 月拾獲較多流浪狗",
         "- 但若是拆成不同年份來看（下圖），就會注意到數據還是受 2023 年影響為主，所以增加的原因也是需要更多資訊才能確定。",
     ]
     col_left_right_component(fig, md_text_list)
-    fig = dog_data_plotter.multi_yearly_plot()
+    fig = dog_data_plotter.yearly_monthly_bar(title="2020 ~ 2023年每月分佈")
     st.plotly_chart(fig, use_container_width=True)
 
 ## 性別分析
-tab_gender, tab_gender_bodytype = st.tabs(["公母比例", "公母 VS. 體型"])
+tab_list = ["公母比例", "公母 VS. 體型"]
+tab_gender, tab_gender_bodytype = st.tabs(tab_list)
 with tab_gender:
-    fig = dog_data_plotter.draw_pie(target_col="animal_sex", title="公母比例")
+    fig = dog_data_plotter.gender_pie(title=tab_list[0])
     md_text_list = ["- 整體公母差異並不大，分佈約是各半"]
     col_left_right_component(fig, md_text_list)
 
 with tab_gender_bodytype:
-    fig = dog_data_plotter.draw_pie(
-        target_col="animal_sex", facet_col="animal_bodytype", title="公母 VS. 體型"
-    )
+    fig = dog_data_plotter.gender_bodytype_pie(title=tab_list[1])
     md_text_list = ["- 中大型犬皆為公狗多於母狗，僅有小型犬的母狗數量大於公狗"]
     col_left_right_component(fig, md_text_list)
 
@@ -67,25 +68,24 @@ tab_list = [
 ]
 tabs = st.tabs(tab_list)
 with tabs[0]:
-    fig = dog_data_plotter.draw_pie("animal_Variety_converted", title="品種比例")
+    fig = dog_data_plotter.breed_pie(title=tab_list[0])
     md_text_list = ["- 幾乎所有流浪狗的品種皆為米克斯"]
     col_left_right_component(fig, md_text_list)
 with tabs[1]:
-    fig = dog_data_plotter.dog_breed_exclude_mix()
+    fig = dog_data_plotter.breed_exclude_mix(title=tab_list[1])
     md_text_list = ["##", "##", "- 排除米克斯後，以貴賓犬、比特犬、柴犬、臘腸、瑪爾濟斯這些台灣常見寵物狗品種為主"]
     col_left_right_component(fig, md_text_list)
 
+## 年齡分析
 tab_list = [
     "年齡比例",
 ]
 tabs = st.tabs(tab_list)
 with tabs[0]:
-    fig = dog_data_plotter.draw_pie("animal_age", "年齡比例")
+    fig = dog_data_plotter.age_pie(title=tab_list[0])
     md_text_list = ["- 流浪狗多數皆已是成犬"]
     col_left_right_component(fig, md_text_list)
-    fig = dog_data_plotter.draw_pie(
-        "animal_age", "體型 VS. 年齡", facet_col="animal_bodytype"
-    )
+    fig = dog_data_plotter.age_bodytype_pie(title="體型 VS. 年齡")
     md_text_list = [
         "- 其中以大型犬的成犬/幼犬比例（98:2）最懸殊",
         "- 代表流浪的大型犬幾乎都已是成犬",
@@ -93,11 +93,12 @@ with tabs[0]:
     ]
     col_left_right_component(fig, md_text_list)
 
+## 毛色分析
 tab_list = [
     "毛色比例",
 ]
 tabs = st.tabs(tab_list)
 with tabs[0]:
-    fig = dog_data_plotter.draw_pie("animal_colour", "毛色比例", color_by_target_col=False)
+    fig = dog_data_plotter.colour_pie(title=tab_list[0])
     md_text_list = ["- 多數為小黑與小黃", "- 前四名都是黑黃色為主的狗，佔了整體的四分之三"]
     col_left_right_component(fig, md_text_list)
--- util/plotter.py
+++ util/plotter.py
@@ -1,115 +1,151 @@
 import plotly.express as px
 import pandas as pd
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 
 
 class DogDataPlotter:
     def __init__(self, df: pd.DataFrame):
         self.df = df
 
-    def yearly_plot(self):
-        year_groupby = self.df.groupby("year", as_index=False).count()
+    def groupby_count_preprocess(
+        self,
+        groupby_cols: list | str,
+        query: str = None,
+        rename_cols_dict: dict = {"animal_id": "Count"},
+    ):
+        if query:
+            count_df = self.df.query(query)
+        else:
+            count_df = self.df.copy()
+        count_df = count_df.groupby(groupby_cols, as_index=False).count()
+        count_df = count_df.rename(columns=rename_cols_dict)
+        return count_df
+
+    def yearly_bar(self, title: str):
+        target_col = "year"
+        count_df = self.groupby_count_preprocess("year")
         fig = px.bar(
-            year_groupby,
-            x="year",
-            y="animal_id",
-            title="每年分佈",
-            # height=400,
+            count_df,
+            x=target_col,
+            y="Count",
+            title=title,
             text_auto=True,
         )
-        fig.update_traces(showlegend=False)
         fig.update_xaxes(dtick=1)
         return fig
 
-    def monthly_plot(self):
-        month_groupby = self.df.groupby("month", as_index=False).count()
-        fig = px.bar(
-            month_groupby, x="month", y="animal_id", title="每月分佈", text_auto=True
-        )
+    def monthly_bar(self, title: str):
+        target_col = "month"
+        count_df = self.groupby_count_preprocess("month")
+        fig = px.bar(count_df, x=target_col, y="Count", title=title, text_auto=True)
         fig.update_xaxes(dtick=1)
         return fig
 
-    def multi_yearly_plot(self):
-        year_groupby = self.df.groupby(["year", "month"], as_index=False).count()
+    def yearly_monthly_bar(self, title: str):
+        target_col = "month"
+        facet_row = "year"
         year_range = list(range(2020, 2024))
+        count_df = self.groupby_count_preprocess(
+            [target_col, facet_row], query=f"year in {year_range}"
+        )
         fig = px.bar(
-            year_groupby.query(f"year in {year_range}"),
-            x="month",
-            y="animal_id",
-            color="animal_id",
-            facet_row="year",
+            count_df,
+            x=target_col,
+            y="Count",
+            title=title,
+            color="Count",
+            facet_row=facet_row,
             facet_row_spacing=0,
             text_auto=True,
         )
-        fig.update_layout(height=600, title_text="2020 ~ 2023年每月分佈")
+        fig.update_layout(height=600)
         fig.update_xaxes(dtick=1)
         fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
         return fig
 
-    def draw_pie(
-        self,
-        target_col: str,
-        title: str,
-        facet_col: str = None,
-        facet_row: str = None,
-        width: int = 600,
-        textfont_size: int = 24,
-        color_by_target_col: bool = True,
-    ):
-        if facet_col or facet_row:
-            if facet_col:
-                groupby_cols = [target_col, facet_col]
-            else:
-                groupby_cols = [target_col, facet_row]
-        else:
-            groupby_cols = target_col
-
-        tmp_groupby_df = self.df.groupby(groupby_cols, as_index=False).count()
-        if color_by_target_col:
-            fig = px.pie(
-                tmp_groupby_df,
-                names=target_col,
-                values="animal_id",
-                facet_col=facet_col,
-                facet_row=facet_row,
-                title=title,
-                width=width,
-                color=target_col,
-            )
-        else:
-            fig = px.pie(
-                tmp_groupby_df,
-                names=target_col,
-                values="animal_id",
-                facet_col=facet_col,
-                facet_row=facet_row,
-                title=title,
-                width=width,
-            )
-        fig.update_traces(textposition="inside", textfont_size=textfont_size)
-        if facet_col or facet_row:
-            fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
-            # fig.for_each_trace(lambda t: t.update(name=t.name.split("=")[1]))
+    def gender_pie(self, title: str):
+        target_col = "animal_sex"
+        count_df = self.groupby_count_preprocess(target_col)
+        fig = px.pie(
+            count_df,
+            names=target_col,
+            values="Count",
+            title=title,
+        )
+        fig.update_traces(textposition="inside", textfont_size=24)
         return fig
 
-    def dog_breed_exclude_mix(self):
-        animal_Variety_exclude_mix = (
-            self.df.query("animal_Variety_converted != '米克斯'")
-            .groupby("animal_Variety_converted", as_index=False)
-            .count()
+    def gender_bodytype_pie(self, title: str):
+        target_col = "animal_sex"
+        facet_col = "animal_bodytype"
+        count_df = self.groupby_count_preprocess([target_col, facet_col])
+        fig = px.pie(
+            count_df, names=target_col, values="Count", title=title, facet_col=facet_col
         )
-        animal_Variety_exclude_mix = animal_Variety_exclude_mix.sort_values(
-            by="animal_id"
+        fig.update_traces(textposition="inside", textfont_size=24)
+        fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
+        return fig
+
+    def breed_pie(self, title: str):
+        target_col = "animal_Variety_converted"
+        count_df = self.groupby_count_preprocess(target_col)
+        fig = px.pie(
+            count_df,
+            names=target_col,
+            values="Count",
+            title=title,
         )
+        fig.update_traces(textposition="inside", textfont_size=24)
+        return fig
+
+    def breed_exclude_mix(self, title: str):
+        target_col = "animal_Variety_converted"
+        count_df = self.groupby_count_preprocess(
+            target_col, query=f"{target_col} != '米克斯'"
+        )
+        count_df = count_df.sort_values(by="Count")
         fig = px.bar(
-            data_frame=animal_Variety_exclude_mix,
-            y="animal_Variety_converted",
-            x="animal_id",
-            title="品種比例（不含米克斯）",
+            count_df,
+            y=target_col,
+            x="Count",
+            title=title,
             orientation="h",
             height=900,
             text_auto=True,
+        )
+        fig.update_traces(textposition="inside", textfont_size=24)
+        return fig
+
+    def age_pie(self, title: str):
+        target_col = "animal_age"
+        count_df = self.groupby_count_preprocess(target_col)
+        fig = px.pie(
+            count_df,
+            names=target_col,
+            values="Count",
+            title=title,
+        )
+        fig.update_traces(textposition="inside", textfont_size=24)
+        return fig
+
+    def age_bodytype_pie(self, title: str):
+        target_col = "animal_age"
+        facet_col = "animal_bodytype"
+        count_df = self.groupby_count_preprocess([target_col, facet_col])
+        fig = px.pie(
+            count_df, names=target_col, values="Count", title=title, facet_col=facet_col
+        )
+        fig.update_traces(textposition="inside", textfont_size=24)
+        fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
+        return fig
+
+    def colour_pie(self, title: str):
+        target_col = "animal_colour"
+        count_df = self.groupby_count_preprocess(target_col)
+        fig = px.pie(
+            count_df,
+            names=target_col,
+            values="Count",
+            title=title,
         )
         fig.update_traces(textposition="inside", textfont_size=24)
         return fig
